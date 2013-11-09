756c28e3fd0ccb511dc91d991ffa7e8f54c97b34
Yuriy Tarnavskiy <etheriq2@gmail.com>
2013-11-09 17:41:08 +0200

    use bootstrap

--- src/Etheriq/Lesson5Bundle/Controller/DefaultController.php
+++ src/Etheriq/Lesson5Bundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('EtheriqLesson5Bundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('EtheriqLesson5Bundle:Default:hello.html.twig', array('name' => $name));
     }
 }
